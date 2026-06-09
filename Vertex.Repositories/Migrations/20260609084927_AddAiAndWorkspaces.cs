83add096ebea36c83735f4aae6d174ff9fe427ed
Nguyễn Quang Tùng <tungnqse183817@fpt.edu.vn>
2026-06-09 15:51:10 +0700

    mig: add migration for AddAiAndWorkspaces

---
+++ Vertex.Repositories/Migrations/20260609084927_AddAiAndWorkspaces.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Vertex.Repositories.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAiAndWorkspaces : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "start_date",
+                table: "tasks",
+                type: "date",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "end_date",
+                table: "tasks",
+                type: "date",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "deadline",
+                table: "projects",
+                type: "date",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_projects_organizations_org_id",
+                table: "projects",
+                column: "org_id",
+                principalTable: "organizations",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_projects_organizations_org_id",
+                table: "projects");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "start_date",
+                table: "tasks",
+                type: "timestamp with time zone",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "date");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "end_date",
+                table: "tasks",
+                type: "timestamp with time zone",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "date");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "deadline",
+                table: "projects",
+                type: "timestamp with time zone",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "date");
+        }
+    }
+}
